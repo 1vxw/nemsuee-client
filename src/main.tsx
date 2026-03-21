bac8662857d3faa380b26377a0165003c7a52bb4
VincePradas <pradasvince@gmail.com>
2026-03-21 21:43:41 +0800

    setup vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,12 @@ import { BrowserRouter } from "react-router-dom";
 import '@fontsource-variable/manrope/index.css'
 import './index.css'
 import App from './App.tsx'
+import { Analytics } from "@vercel/analytics/next"
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <App />
+    <Analytics />
   </BrowserRouter>,
 )
 
